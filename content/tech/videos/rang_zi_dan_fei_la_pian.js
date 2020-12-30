628858fdffc943436b716585f663232513f3f149
BORBER <borber.drum@gmail.com>
2020-12-30 13:33:40 +0800

    http2https

--- content/tech/videos/rang_zi_dan_fei_la_pian.js
+++ content/tech/videos/rang_zi_dan_fei_la_pian.js
@@ -1,7 +1,7 @@
 const dp = new DPlayer({
     container: document.getElementById("dplayer"),
     video: {
-    url:"http://aliregistry-cn-shanghai.oss-cn-shanghai.aliyuncs.com/docker/registry/v2/blobs/sha256/45/45c46393a808b2e5eaf49111aa920a8df9afaeaf78278ff54f41cf3cb92c27bc/data?Expires=1609305624&OSSAccessKeyId=LTAI4FyN99sZuwGQsaDkPxbv&Signature=vB87SP8j6jGPABXtg1a6hmNrJj0%3D",
+    url:"https://aliregistry-cn-shanghai.oss-cn-shanghai.aliyuncs.com/docker/registry/v2/blobs/sha256/45/45c46393a808b2e5eaf49111aa920a8df9afaeaf78278ff54f41cf3cb92c27bc/data?Expires=1609305624&OSSAccessKeyId=LTAI4FyN99sZuwGQsaDkPxbv&Signature=vB87SP8j6jGPABXtg1a6hmNrJj0%3D",
     type: "customHls",
     customType: {
         customHls: function (video, player) {
