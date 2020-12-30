30ed56f938ea07841824fe27363a0514df548c93
BORBER <borber.drum@gmail.com>
2020-12-30 12:54:06 +0800

    "静态博客

---
+++ content/tech/videos/rang_zi_dan_fei_la_pian.js
@@ -0,0 +1,14 @@
+const dp = new DPlayer({
+    container: document.getElementById("dplayer"),
+    video: {
+    url:"http://aliregistry-cn-shanghai.oss-cn-shanghai.aliyuncs.com/docker/registry/v2/blobs/sha256/45/45c46393a808b2e5eaf49111aa920a8df9afaeaf78278ff54f41cf3cb92c27bc/data?Expires=1609305624&OSSAccessKeyId=LTAI4FyN99sZuwGQsaDkPxbv&Signature=vB87SP8j6jGPABXtg1a6hmNrJj0%3D",
+    type: "customHls",
+    customType: {
+        customHls: function (video, player) {
+        const hls = new Hls();
+        hls.loadSource(video.src);
+        hls.attachMedia(video);
+        },
+    },
+    },
+});
